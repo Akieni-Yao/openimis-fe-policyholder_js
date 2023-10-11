5670146d4c79b8221934757f06d43639a60227ea
Eric Darchis <darchis@pobox.com>
2023-10-11 09:36:34 +0200

    Add employee excel upload

--- src/components/PolicyHolderInsureesTab.js
+++ src/components/PolicyHolderInsureesTab.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
-import { Tab, Grid, Typography } from "@material-ui/core";
-import { formatMessage, PublishedComponent, FormattedMessage } from "@openimis/fe-core";
+import { Tab, Grid, Typography, Input, Button } from "@material-ui/core";
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import { formatMessage, PublishedComponent, FormattedMessage, baseApiUrl, apiHeaders } from "@openimis/fe-core";
 import {
     RIGHT_POLICYHOLDERINSUREE_CREATE,
     RIGHT_POLICYHOLDERINSUREE_SEARCH,
@@ -44,8 +45,43 @@ class PolicyHolderInsureesTabPanel extends Component {
         }));
     }
 
+    onUpload = async (event) => {
+        const { policyHolder } = this.props;
+        const file = event.target.files[0];
+        let formData = new FormData();
+        formData.append("file", file);
+
+        let encodedCode = encodeURIComponent(policyHolder.code);
+        let url_import = `${baseApiUrl}/policyholder/imports/${encodedCode}/policyholderinsurees`;
+
+        try {
+          const response = await fetch(url_import, {
+            headers: apiHeaders,
+            body: formData,
+            method: "POST",
+            credentials: "same-origin",
+          });
+
+          const payload = await response.text();
+
+          if (response.status >= 400) {
+            // alert(`Error ${response.status}: ${payload.error}`);
+            alert(`Error ${response.status}: ${payload}`);
+            return;
+          }
+          alert("Success");
+        } catch (error) {
+            alert(
+              error?.message ??
+              formatMessage(
+                `An error occurred. Please contact your administrator. ${error?.message}`
+              )
+            );
+        }
+    };
+
     render() {
-        const { rights, value, isTabsEnabled, policyHolder } = this.props;
+        const { rights, value, isTabsEnabled, policyHolder, intl } = this.props;
         return (
             (rights.includes(RIGHT_POLICYHOLDERINSUREE_SEARCH) ||
                 rights.includes(RIGHT_PORTALPOLICYHOLDERINSUREE_SEARCH)) && (
@@ -61,10 +97,34 @@ class PolicyHolderInsureesTabPanel extends Component {
                                 rights.includes(RIGHT_PORTALPOLICYHOLDERINSUREE_CREATE)) && (
                                 <Grid
                                     container
-                                    justify="flex-end"
+                                    justifyContent="flex-start"
                                     alignItems="center"
                                     spacing={1}
                                 >
+                                    <Input
+                                        required
+                                        id="import-button"
+                                        style={{ display: 'none' }}
+                                        inputProps={{
+                                          accept:
+                                            ".xls, application/vnd.ms-excel, .xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                        }}
+                                        type="file"
+                                        onChange={this.onUpload}
+                                    />
+                                    <label htmlFor="import-button">
+                                        <Button
+                                            variant="contained"
+                                            component="span"
+                                            color="primary"
+                                            startIcon={<CloudUploadIcon />}
+                                        >
+                                            <FormattedMessage
+                                                module="policyHolder"
+                                                id="policyHolderInsuree.import"
+                                            />
+                                        </Button>
+                                    </label>
                                     <Grid item>
                                         <Typography>
                                             <FormattedMessage
